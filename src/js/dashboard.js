2c16629d57793aa57b2db6eba4329f0142ef4d86
José Antonio Medina Martinez <joseamm@comunidad.unam.mx>
2024-08-12 13:49:00 -0600

    Update: Inializacion de responsive personla_cre.html

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -43,18 +43,22 @@ new Chart(lineal, {
         label: "# de Electrolineras",
         data: [
           12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3,
-          5, 2, 3, 12, 19, 3, 5, 2, 3,
+          5, 2, 3, 12, 19, 3, 5, 2, 3, 5, 9,
         ],
         borderWidth: 1,
+        borderRadius: 5,
+        borderSkipped: false,
       },
 
       {
         label: "# de Electroterminales",
         data: [
           8, 15, 7, 10, 4, 6, 8, 15, 7, 10, 4, 6, 8, 15, 7, 10, 4, 6, 8, 15, 7,
-          10, 4, 6, 8, 15, 7, 10, 4, 6,
+          10, 4, 6, 8, 15, 7, 10, 4, 6, 2, 9,
         ],
         borderWidth: 1,
+        borderRadius: 5,
+        // borderSkipped: false,
       },
     ],
   },
@@ -80,6 +84,11 @@ new Chart(lineal, {
     plugins: {
       legend: {
         labels: {
+          usePointStyle: true, // Cambia los rectángulos a puntos (círculos)
+          pointStyle: "circle", // Otras opciones: 'circle', 'rect', 'rectRounded', 'rectRot', 'triangle', 'star', 'line', 'dash'
+          boxWidth: 15, // Ancho de la caja (rectángulo)
+          boxHeight: 15, // Altura de la caja (aplica a ciertas formas)
+          padding: 20, // Espaciado entre el rectángulo y el texto
           font: {
             size: 12, // Tamaño de la fuente para las etiquetas de la leyenda
           },
@@ -89,49 +98,48 @@ new Chart(lineal, {
   },
 });
 
-
 const data = {
-  labels: ['Aprobadas', 'En revisión', 'Sin revisar', 'Con problemas'],
+  labels: ["Aprobadas", "En revisión", "Sin revisar", "Con problemas"],
   datasets: [
     {
-      label: 'Electrolinera',
+      label: "Electrolinera",
       data: [12, 19, 3, 5], // Valores numéricos para cada categoría
       backgroundColor: [
-        'rgba(75, 192, 192, 0.6)',  // Aprobadas
-        'rgba(255, 205, 86, 0.6)',  // En revisión
-        'rgba(54, 162, 235, 0.6)',   // Con problemas
-        'rgba(255, 99, 132, 0.6)',  // Sin revisar
+        "rgba(75, 192, 192, 0.6)", // Aprobadas
+        "rgba(255, 205, 86, 0.6)", // En revisión
+        "rgba(54, 162, 235, 0.6)", // Con problemas
+        "rgba(255, 99, 132, 0.6)", // Sin revisar
       ],
       borderColor: [
-        'rgba(75, 192, 192, 1)',
-        'rgba(255, 205, 86, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 99, 132, 1)',
+        "rgba(75, 192, 192, 1)",
+        "rgba(255, 205, 86, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 99, 132, 1)",
       ],
-      borderWidth: 1
+      borderWidth: 1,
     },
-    // {
-    //   label: 'Electroterminal',
-    //   data: [8, 23, 4, 2], // Valores numéricos para cada categoría
-    //   backgroundColor: [
-    //     'rgba(75, 192, 192, 0.6)',  // Aprobadas
-    //     'rgba(255, 205, 86, 0.6)',  // En revisión
-    //     'rgba(54, 162, 235, 0.6)',   // Con problemas
-    //     'rgba(255, 99, 132, 0.6)',  // Sin revisar
-    //   ],
-    //   borderColor: [
-    //     'rgba(75, 192, 192, 1)',
-    //     'rgba(255, 205, 86, 1)',
-    //     'rgba(54, 162, 235, 1)',
-    //     'rgba(255, 99, 132, 1)',
-    //   ],
-    //   borderWidth: 1
-    // }
-  ]
+    {
+      label: "Electroterminal",
+      data: [8, 23, 4, 2], // Valores numéricos para cada categoría
+      backgroundColor: [
+        "rgba(75, 192, 192, 0.6)", // Aprobadas
+        "rgba(255, 205, 86, 0.6)", // En revisión
+        "rgba(54, 162, 235, 0.6)", // Con problemas
+        "rgba(255, 99, 132, 0.6)", // Sin revisar
+      ],
+      borderColor: [
+        "rgba(75, 192, 192, 1)",
+        "rgba(255, 205, 86, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 99, 132, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
 };
 
 const config = {
-  type: "polarArea",
+  type: "pie",
   data: data,
   options: {
     responsive: true,
@@ -139,25 +147,29 @@ const config = {
       legend: {
         position: "top",
         labels: {
+          usePointStyle: true, // Cambia los rectángulos a puntos (círculos)
+          pointStyle: "circle", // Otras opciones: 'circle', 'rect', 'rectRounded', 'rectRot', 'triangle', 'star', 'line', 'dash'
+          boxWidth: 15, // Ancho de la caja (rectángulo)
+          boxHeight: 15, // Altura de la caja (aplica a ciertas formas)
+          padding: 20, // Espaciado entre el rectángulo y el texto
           font: {
-            size: 12,  // Tamaño de la fuente de la leyenda
-            family: 'Arial',
-            weight: 'bold'
-          }
-        }
+            size: 12, // Tamaño de la fuente de la leyenda
+            family: "Arial",
+            weight: "bold",
+          },
+        },
       },
       title: {
         display: true,
         text: "Estatus Estaciones de Carga",
         font: {
-          size: 12,  // Tamaño de la fuente del título
-          family: 'Arial',
-          weight: 'bold'
-        }
-      }
-    }
-  }
+          size: 12, // Tamaño de la fuente del título
+          family: "Arial",
+          weight: "bold",
+        },
+      },
+    },
+  },
 };
 
 new Chart(dona, config);
-
--- src/js/tabla.js
+++ src/js/tabla.js
@@ -1,1 +1,21 @@
-let borrar = document.getElementById("borrar")
+let habilitar = document.querySelectorAll(".habilitar");
+let borrar = document.querySelectorAll(".borrar");
+
+habilitar.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    if (this.src.includes("deshabilitar__red.svg")) {
+      this.src = "../../public/images/habilitar__green.svg";
+    } else {
+      this.src = "../../public/images/deshabilitar__red.svg";
+    }
+  });
+});
+
+borrar.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    var myModal = new bootstrap.Modal(document.getElementById("exampleModal"), {
+      keyboard: false,
+    });
+    myModal.show();
+  });
+});
